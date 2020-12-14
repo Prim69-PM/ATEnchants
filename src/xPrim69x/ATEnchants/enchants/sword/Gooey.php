663782b521de309a1bee2d0eba6e7cdf6a788fe5
THXC <cwpenguinmaster2005@gmail.com>
2020-12-14 13:22:33 -0500

    GOOEY POG

---
+++ src/xPrim69x/ATEnchants/enchants/sword/Gooey.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace xPrim69x\ATEnchants\enchants\sword;
+
+use pocketmine\entity\Entity;
+use pocketmine\entity\Living;
+use pocketmine\item\enchantment\MeleeWeaponEnchantment;
+use pocketmine\Player;
+use xPrim69x\ATEnchants\Main;
+
+class BleedEnchant extends MeleeWeaponEnchantment {
+
+	public function isApplicableTo(Entity $victim) : bool{
+		return $victim instanceof Living;
+	}
+
+	public function getDamageBonus(int $enchantmentLevel) : float{
+		return 0;
+	}
+
+	public function onPostAttack(Entity $d, Entity $player, int $enchantmentLevel) : void{
+    //TODO: Do this
+	}
+}
